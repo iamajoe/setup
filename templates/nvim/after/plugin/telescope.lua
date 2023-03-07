cf7e2ea95a6884bff74d49647771e8c221492c7a
Joel Santos <joe@joesantos.io>
2023-03-07 11:22:13 +0000

    improve grepping with telescope

--- templates/nvim/after/plugin/telescope.lua
+++ templates/nvim/after/plugin/telescope.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Find files in git" })
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, { desc = "Grep and find files" })
+vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
--- templates/nvim/lua/user/packer.lua
+++ templates/nvim/lua/user/packer.lua
@@ -8,8 +8,13 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+	  requires = { 
+      {'nvim-lua/plenary.nvim'},
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("live_grep_args")
+    end
   }
 
   use 'Mofiqul/dracula.nvim'
