168aa0ee3315b4ea352f54efbbbeed4c11395bf5
Joel Santos <joe@joesantos.io>
2023-04-10 12:12:18 +0100

    copy file under netrw

--- templates/nvim/lua/user/remap.lua
+++ templates/nvim/lua/user/remap.lua
@@ -4,10 +4,10 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>km", ":Telescope keymaps<CR>")
 
 -- Move lines up and down
--- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move line down in visual" })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv==", { desc = "move line down in visual" })
--- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move line up in visual" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv==", { desc = "move line up in visual" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move line down in visual" })
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv==", { desc = "move line down in visual" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move line up in visual" })
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv==", { desc = "move line up in visual" })
 
 -- Join lines
 vim.keymap.set("n", "J", "mzJ`z", { desc = "join lines" })
@@ -41,7 +41,19 @@ vim.keymap.set("n", "<leader>bl", ":Telescope buffers<CR>", { desc = "list buffe
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "search and replace cursor word" })
 
 -- Copy current file
-vim.keymap.set("n", "<leader>cf", ":silent exec \"!cp '%:p' '%:p:h/%:t:r.bkp.%:e'\"<cr>", { desc = "duplicate current file"})
+vim.keymap.set("n", "<leader>cf", "", {
+  callback = function()
+    if vim.bo.filetype == "netrw" then
+      vim.cmd("let netrw_selected_file=getcwd() .. '/' .. netrw#Call('NetrwGetWord')")
+      vim.cmd("let netrw_copy_to_file=input('Copy to > ', netrw_selected_file .. '.bkp')")
+      vim.cmd("silent exec '!cp ' .. netrw_selected_file .. ' ' .. netrw_copy_to_file")
+      vim.cmd("e .")
+    else
+      vim.cmd [[silent exec "!cp '%:p' '%:p:h/%:t:r.bkp.%:e'"]]
+    end
+  end,
+  desc = "duplicate current file"
+})
 
 vim.keymap.set("n", "p", "]p") -- indent pasted text
 vim.keymap.set("v", ">", ">gv") -- keep indented text selected
