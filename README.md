765f7c7e73945dbdfe27e1d9e73b10da6f0458f0
Joel Santos <joe@joesantos.io>
2025-02-15 16:03:25 +0000

    update setup folder

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Setup
 
 Ansible personal setup.
-To run:
+
+## Install
+
+- [Ansible](https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html)
+
+## Run
+
+### With .env file
 
 ```sh
+cp .env.dist .env # Don't forget to change the vars
 sh ./run.sh
 ```
--- run.sh
+++ run.sh
@@ -1,26 +1,64 @@
 #!/usr/bin/env bash
 
 usage="Usage: /bin/bash run.sh <service> [params]\n"
-usage="${usage}\n | SERVICE              | PARAMS     |"
-usage="${usage}\n | =====================|=========== |"
-usage="${usage}\n | dev                  | <ssh-user> |"
-usage="${usage}\n | donkey               | <ssh-user> |"
+usage="${usage}\n | PARAMS                        |"
+usage="${usage}\n |===============================|"
+usage="${usage}\n | <target> <ssh-user> [ip_addr] |"
 usage="${usage}\n"
 
-if [ $1 = "help" ]; then
+# check if the .env file exists, then load it
+ENV_FILE=".env"
+if [[ -f "$ENV_FILE" ]]; then
+  echo "Loading environment variables from $ENV_FILE..."
+  set -o allexport  # Enable auto-export
+  source "$ENV_FILE"
+  set +o allexport  # Disable auto-export
+else
+  echo "Env file not found"
+fi
+
+######################################
+# set the variables 
+#
+SERVICE="${SERVICE:-$1}"
+USERNAME="${USERNAME:-$2}"
+USER_EMAIL="${USER_EMAIL:-""}"
+USER_FULL_NAME="${USER_FULL_NAME:-""}"
+
+PLAYBOOK_FOLDER="${PLAYBOOK_FOLDER:-"./playbooks"}"
+
+SSH_SRC_DIR="${SSH_SRC_DIR:-$HOME/.ssh}"
+INVENTORY_PATH="${INVENTORY_PATH:-./inventory/hosts}"
+INVENTORY="${INVENTORY:-"-i $INVENTORY_PATH"}"
+TARGET="${TARGET:-all}"
+SUBSET="${SUBSET:-""}"
+IP_ADDR="${IP_ADDR:-$3}"
+
+######################################
+
+# do we have enough?
+if [[ -z "$SERVICE" || "$SERVICE" == "help" ]]; then
   echo "\n$usage"
   exit
 fi
 
-echo "Deploying $1..."
+# has the user passed in a subset ip address?
+if [[ -n "$IP_ADDR" ]]; then
+  INVENTORY_PATH="./inventory/tmphosts";
+  SUBSET="$IP_ADDR";
+  TARGET="$SERVICE";
 
-if [ $1 = "dev" ]; then
-  ansible-galaxy install abdennour.golang
-  ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./playbooks/dev.yml --user $2 --ask-pass --ask-become-pass -i ./inventory/hosts
+  echo -e "[$TARGET]\n$SUBSET" > $INVENTORY_PATH;
+  INVENTORY="-i $INVENTORY_PATH -l $SUBSET";
 fi
 
-if [ $1 = "donkey" ]; then
-  ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./playbooks/donkey.yml --user $2 --ask-pass --ask-become-pass -i ./inventory/hosts
+# deploy...
+echo "Deploying $SERVICE to $USERNAME:$SUBSET...";
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $PLAYBOOK_FOLDER/$SERVICE.yml --user $USERNAME --ask-pass --ask-become-pass $INVENTORY -e="ssh_src_dir=$SSH_SRC_DIR target=$TARGET git_user_email=$USER_EMAIL git_user_fullname=$USER_FULL_NAME"
+
+# cleanup tmps
+if [[ -n "$IP_ADDR" ]]; then
+  rm -rf $INVENTORY_PATH;
 fi
 
 echo ""
