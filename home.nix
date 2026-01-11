80a17252ef1af044b0f31234a8d008caa3b770ac
Joel Santos <joe@joesantos.io>
2026-01-11 19:37:19 +0000

    feat: add steam

--- home.nix
+++ home.nix
@@ -198,6 +198,16 @@ in
   };
 
   #
+  # ─── Steam ────────────────────────────────────────────────────────────────
+  #
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  #
   # ─── Dependencies ────────────────────────────────────────────────────────────────
   #
 
