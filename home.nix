4c865f73ca1c746f41113a1ad232aaf470ef8df4
Joel Santos <joe@joesantos.io>
2026-01-12 12:29:52 +0000

    feat: move rofi to nixos

--- home.nix
+++ home.nix
@@ -280,13 +280,278 @@ in
       -o "$HOME/.config/alacritty/alacritty_dev.toml"
   '';
 
-  home.activation.getRofiConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p "$HOME/.config/rofi"
-    rm -f $HOME/.config/rofi/*
-    ${pkgs.curl}/bin/curl -sSL \
-      https://raw.githubusercontent.com/iamajoe/setup/refs/heads/master/templates/rofi \
-      -o "$HOME/.config/rofi/config.rasi"
-  '';
+  # Rofi - application launcher
+  programs.rofi = {
+    enable = true;
+    terminal = "${pkgs.alacritty}/bin/alacritty";
+    extraConfig = {
+      modi = "run,drun,window";
+      icon-theme = "Oranchelo";
+      show-icons = false;
+      drun-display-format = "{icon} {name}";
+      location = 0;
+      disable-history = false;
+      hide-scrollbar = true;
+      display-drun = "";
+      dpi = 220;
+    };
+    theme = let inherit (config.lib.formats.rasi) mkLiteral; in {
+      "*" = {
+        bg-col = mkLiteral "#1e1e2e";
+        bg-col-light = mkLiteral "#1e1e2e";
+        border-col = mkLiteral "#1e1e2e";
+        selected-col = mkLiteral "#1e1e2e";
+        blue = mkLiteral "#89b4fa";
+        fg-col = mkLiteral "#cdd6f4";
+        fg-col2 = mkLiteral "#f38ba8";
+        grey = mkLiteral "#6c7086";
+        
+        width = 1200;
+        font = "NotoSansM Nerd Font 9";
+      };
+      
+      "element-text, element-icon, mode-switcher" = {
+        background-color = mkLiteral "inherit";
+        text-color = mkLiteral "inherit";
+      };
+      
+      window = {
+        height = mkLiteral "360px";
+        border = mkLiteral "3px";
+        border-color = mkLiteral "@border-col";
+        background-color = mkLiteral "@bg-col";
+      };
+      
+      mainbox = {
+        background-color = mkLiteral "@bg-col";
+      };
+      
+      inputbar = {
+        children = map mkLiteral ["prompt" "entry"];
+        background-color = mkLiteral "@bg-col";
+        border-radius = mkLiteral "5px";
+        padding = mkLiteral "2px";
+      };
+      
+      prompt = {
+        background-color = mkLiteral "@blue";
+        padding = mkLiteral "6px";
+        text-color = mkLiteral "@bg-col";
+        border-radius = mkLiteral "3px";
+        margin = mkLiteral "20px 0px 0px 20px";
+      };
+      
+      "textbox-prompt-colon" = {
+        expand = false;
+        str = ":";
+      };
+      
+      entry = {
+        padding = mkLiteral "6px";
+        margin = mkLiteral "20px 0px 0px 10px";
+        text-color = mkLiteral "@fg-col";
+        background-color = mkLiteral "@bg-col";
+      };
+      
+      listview = {
+        border = mkLiteral "0px 0px 0px";
+        padding = mkLiteral "6px 0px 0px";
+        margin = mkLiteral "10px 0px 0px 20px";
+        columns = 2;
+        lines = 5;
+        background-color = mkLiteral "@bg-col";
+      };
+      
+      element = {
+        padding = mkLiteral "5px";
+        background-color = mkLiteral "@bg-col";
+        text-color = mkLiteral "@fg-col";
+      };
+      
+      element-icon = {
+        size = mkLiteral "25px";
+      };
+      
+      "element selected" = {
+        background-color = mkLiteral "@selected-col";
+        text-color = mkLiteral "@fg-col2";
+      };
+      
+      mode-switcher = {
+        spacing = 0;
+      };
+      
+      button = {
+        padding = mkLiteral "10px";
+        background-color = mkLiteral "@bg-col-light";
+        text-color = mkLiteral "@grey";
+        vertical-align = mkLiteral "0.5";
+        horizontal-align = mkLiteral "0.5";
+      };
+      
+      "button selected" = {
+        background-color = mkLiteral "@bg-col";
+        text-color = mkLiteral "@blue";
+      };
+      
+      message = {
+        background-color = mkLiteral "@bg-col-light";
+        margin = mkLiteral "2px";
+        padding = mkLiteral "2px";
+        border-radius = mkLiteral "5px";
+      };
+      
+      textbox = {
+        padding = mkLiteral "6px";
+        margin = mkLiteral "20px 0px 0px 20px";
+        text-color = mkLiteral "@blue";
+        background-color = mkLiteral "@bg-col-light";
+      };
+    };
+  };
+
+  # Dunst - notification daemon
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        monitor = 0;
+        follow = "mouse";
+        width = 300;
+        height = 300;
+        origin = "top-right";
+        offset = "10x50";
+        scale = 0;
+        notification_limit = 5;
+        
+        progress_bar = true;
+        progress_bar_height = 10;
+        progress_bar_frame_width = 1;
+        progress_bar_min_width = 150;
+        progress_bar_max_width = 300;
+        
+        indicate_hidden = "yes";
+        transparency = 10;
+        separator_height = 2;
+        padding = 8;
+        horizontal_padding = 8;
+        text_icon_padding = 0;
+        frame_width = 2;
+        frame_color = "#89B4FA";
+        gap_size = 5;
+        separator_color = "frame";
+        sort = "yes";
+        
+        font = "Noto Sans 12";
+        line_height = 0;
+        markup = "full";
+        format = "<b>%s</b>\\n%b";
+        alignment = "left";
+        vertical_alignment = "center";
+        show_age_threshold = 60;
+        ellipsize = "middle";
+        ignore_newline = "no";
+        stack_duplicates = true;
+        hide_duplicate_count = false;
+        show_indicators = "yes";
+        
+        icon_position = "left";
+        min_icon_size = 32;
+        max_icon_size = 128;
+        
+        sticky_history = "yes";
+        history_length = 20;
+        
+        browser = "${pkgs.firefox}/bin/firefox";
+        always_run_script = true;
+        title = "Dunst";
+        class = "Dunst";
+        corner_radius = 10;
+        ignore_dbusclose = false;
+        
+        mouse_left_click = "close_current";
+        mouse_middle_click = "do_action, close_current";
+        mouse_right_click = "close_all";
+      };
+      
+      # Catppuccin Mocha theme colors
+      urgency_low = {
+        background = "#1E1E2E";
+        foreground = "#CDD6F4";
+        frame_color = "#89B4FA";
+        timeout = 5;
+      };
+      
+      urgency_normal = {
+        background = "#1E1E2E";
+        foreground = "#CDD6F4";
+        frame_color = "#89B4FA";
+        timeout = 10;
+      };
+      
+      urgency_critical = {
+        background = "#1E1E2E";
+        foreground = "#CDD6F4";
+        frame_color = "#F38BA8";
+        timeout = 0;
+      };
+    };
+  };
+
+  # Picom - compositor for X11
+  services.picom = {
+    enable = true;
+    backend = "glx";
+    vSync = true;
+    
+    fade = true;
+    fadeSteps = [ 0.03 0.03 ];
+    fadeDelta = 10;
+    
+    shadow = true;
+    shadowOpacity = 0.75;
+    shadowOffsets = [ (-15) (-15) ];
+    shadowExclude = [
+      "name = 'Notification'"
+      "class_g = 'Conky'"
+      "class_g ?= 'Notify-osd'"
+      "class_g = 'Cairo-clock'"
+      "_GTK_FRAME_EXTENTS@:c"
+    ];
+    
+    settings = {
+      # Rounded corners
+      corner-radius = 10;
+      rounded-corners-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+      ];
+      
+      # Opacity rules
+      inactive-opacity = 0.95;
+      active-opacity = 1.0;
+      frame-opacity = 0.9;
+      
+      # Blur
+      blur-method = "dual_kawase";
+      blur-strength = 5;
+      blur-background = true;
+      blur-background-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+        "_GTK_FRAME_EXTENTS@:c"
+      ];
+      
+      # Performance
+      mark-wmwin-focused = true;
+      mark-ovredir-focused = true;
+      detect-rounded-corners = true;
+      detect-client-opacity = true;
+      detect-transient = true;
+      use-damage = true;
+      log-level = "warn";
+    };
+  };
 
   #
   # ─── Dependencies ────────────────────────────────────────────────────────────────
@@ -337,8 +602,8 @@ in
     # System GUI tools
     blueman         # bluetooth manager GUI
     gparted         # GUI partition editor
+    flameshot       # screenshot tool
 
-    rofi # application launcher
     xclip           # clipboard utilities
     xdotool         # X11 automation for better VM integration
 
