40f8025105b66f28e66afd443a70109562744771
Joel Santos <joe@joesantos.io>
2026-06-18 19:55:09 +0100

    fix: installation of es-de

--- templates/emulation/es-de.sh
+++ templates/emulation/es-de.sh
@@ -13,16 +13,10 @@ mkdir -p "$(dirname "$LOG")"
   echo "Launching ES-DE"
   echo "Date: $(date)"
   echo "ESDE_APPIMAGE: $ESDE_APPIMAGE"
-  echo "ROMS_DIR: $ROMS_DIR"
-  echo "BIOS_DIR: $BIOS_DIR"
   echo "HOME: $HOME"
   echo "---"
 } > "$LOG"
 
 unset LD_PRELOAD
 
-exec @appImageRun@ "$ESDE_APPIMAGE" \
-  --home "$HOME" \
-  --roms "$ROMS_DIR" \
-  --systems "$BIOS_DIR" \
-  >> "$LOG" 2>&1
+exec @appImageRun@ "$ESDE_APPIMAGE" --home "$HOME" >> "$LOG" 2>&1
