1d355de33f3ae28bbbf3b634a039b6662774f433
Joel Santos <joe@joesantos.io>
2026-06-18 19:43:19 +0100

    fix: better es-de script

--- templates/emulation/es-de.sh
+++ templates/emulation/es-de.sh
@@ -3,15 +3,26 @@ set -euo pipefail
 
 LOG="@steamShortcutsDir@/es-de.log"
 ESDE_APPIMAGE="@esDeAppImage@"
+
 export ROMS_DIR="@romsDir@"
 export BIOS_DIR="@biosDir@"
 
-echo "Launching ES-DE" > "\$LOG"
-echo "Date: \$(date)" >> "\$LOG"
+mkdir -p "$(dirname "$LOG")"
+
+{
+  echo "Launching ES-DE"
+  echo "Date: $(date)"
+  echo "ESDE_APPIMAGE: $ESDE_APPIMAGE"
+  echo "ROMS_DIR: $ROMS_DIR"
+  echo "BIOS_DIR: $BIOS_DIR"
+  echo "HOME: $HOME"
+  echo "---"
+} > "$LOG"
+
 unset LD_PRELOAD
 
-exec @appImageRun@ "\$ESDE_APPIMAGE" \
+exec @appImageRun@ "$ESDE_APPIMAGE" \
   --home "$HOME" \
-  --roms "@romsDir@" \
-  --systems "@biosDir@" \
-  >> "\$LOG" 2>&1
+  --roms "$ROMS_DIR" \
+  --systems "$BIOS_DIR" \
+  >> "$LOG" 2>&1
