9e9a85e90bb1950e7a7e10f62127cc15cb1ce682
Joel Santos <joe@joesantos.io>
2026-07-13 09:53:32 +0100

    fix: remove hdmi force audio

--- programs/audio.nix
+++ programs/audio.nix
@@ -18,19 +18,19 @@ in
   # Force audio output to the HDMI card if audio doesn't default there on
   # its own (TV setups). Find the right card name with `pactl list cards short`
   # and swap it into the grep below, then uncomment.
-  systemd.user.services.force-hdmi-audio = {
-    description = "Force HDMI audio profile";
-    wantedBy = [ "default.target" ];
-    after = [ "pipewire.service" "wireplumber.service" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = let
-        script = pkgs.writeShellScript "force-hdmi-audio" ''
-          CARD="$( ${pkgs.pulseaudio}/bin/pactl list cards short | ${pkgs.gnugrep}/bin/grep 'alsa_card.pci-XXXX_XX_XX.X' | ${pkgs.coreutils}/bin/cut -f1 )"
-          [ -n "$CARD" ] && exec ${pkgs.pulseaudio}/bin/pactl set-card-profile "$CARD" output:hdmi-stereo
-        '';
-      in "${script}";
-    };
-  };
+  # systemd.user.services.force-hdmi-audio = {
+  #   description = "Force HDMI audio profile";
+  #   wantedBy = [ "default.target" ];
+  #   after = [ "pipewire.service" "wireplumber.service" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = let
+  #       script = pkgs.writeShellScript "force-hdmi-audio" ''
+  #         CARD="$( ${pkgs.pulseaudio}/bin/pactl list cards short | ${pkgs.gnugrep}/bin/grep 'alsa_card.pci-XXXX_XX_XX.X' | ${pkgs.coreutils}/bin/cut -f1 )"
+  #         [ -n "$CARD" ] && exec ${pkgs.pulseaudio}/bin/pactl set-card-profile "$CARD" output:hdmi-stereo
+  #       '';
+  #     in "${script}";
+  #   };
+  # };
 }
 
