4d3ad429be784e3b104bbb23f831b8592b68f20e
Joel <joe@joesantos.io>
2026-07-27 11:52:26 +0100

    feat: herdr config template

--- programs/herdr.nix
+++ programs/herdr.nix
@@ -3,8 +3,7 @@
 let
   inherit (userConfig) username homeDir;
 
-  # TODO: change the templates
-  herdrTemplate = ../templates/tmux;
+  herdrTemplate = ../templates/herdr;
 in
 {
   environment.systemPackages = [
@@ -12,21 +11,11 @@ in
   ];
 
   system.activationScripts.herdrConfig.text = ''
-    # install -d -m 0755 -o ${username} -g users ${homeDir}
-    # install -d -m 0755 -o ${username} -g users ${homeDir}/.config
+    install -d -m 0755 -o ${username} -g users ${homeDir}
+    install -d -m 0755 -o ${username} -g users ${homeDir}/.config
 
-    # rm -f ${homeDir}/.tmux.conf
-    # rm -rf ${homeDir}/.config/tmux
-
-    # printf '%s\n' 'run-shell "tmux source-file ~/.config/tmux/main.conf"' > ${homeDir}/.tmux.conf
-
-    # cp -r ${herdrTemplate} ${homeDir}/.config/herdr
-
-    # chown ${username}:users ${homeDir}/.tmux.conf
-    # chmod 0644 ${homeDir}/.tmux.conf
-
-    # chown -R ${username}:users ${homeDir}/.config/tmux
-    # find ${homeDir}/.config/tmux -type d -exec chmod 0755 {} \;
-    # find ${homeDir}/.config/tmux -type f -exec chmod 0644 {} \;
+    rm -rf ${homeDir}/.config/herdr/config.toml
+    cp -r ${herdrTemplate}/config.toml ${homeDir}/.config/herdr/config.toml
+    chown -R ${username}:users ${homeDir}/.config/herdr/config.toml
   '';
 }
